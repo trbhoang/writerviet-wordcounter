8d859fc039983273b068e718ede94be8e88eec0a
Hoang Tran <trbhoang@gmail.com>
2020-02-24 23:24:09 +0700

    plain bootstrap ui

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,15 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Nav from "../components/Nav.svelte";
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+</svelte:head>
 
-<Nav {segment}/>
+<Nav />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,77 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  .box {
+    background-color: #fff;
+    border: 1px solid #d1d5da;
+    border-radius: 3px;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  .footer {
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    background-color: #f8f9fa;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  .footer p {
+    font-weight: 600;
+    color: #495057;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Word counter</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="container">
+  <div class="row">
+    <div class="col py-5">
+      <h1 class="text-primary">Word Counter</h1>
+      <p class="text-secondary">
+        Công cụ phân tích và thống kê văn bản.
+        <br />
+        Nhập văn bản vào ô bên dưới, công cụ sẽ thống kê số từ, câu, đoạn
+        văn,...
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-9 pb-5">
+      <textarea class="form-control box" rows="10" />
+    </div>
+    <div class="col-md-3">
+      <div class="row p-1 mb-1">
+        <div class="h6 col-12 text-primary">Thống kê</div>
+      </div>
+      <div class="row p-2 mb-1">
+        <div class="col-8 text-dark">Paragraphs</div>
+        <div class="col-4">
+          <span class="text-secondary">1,098</span>
+        </div>
+      </div>
+      <div class="row p-2 mb-1">
+        <div class="col-8 text-dark">Sentences</div>
+        <div class="col-4">
+          <span class="text-secondary">1,098</span>
+        </div>
+      </div>
+      <div class="row p-2 mb-1">
+        <div class="col-8 text-dark">Words</div>
+        <div class="col-4">
+          <span class="text-secondary">1,098</span>
+        </div>
+      </div>
+      <div class="row p-2 mb-1">
+        <div class="col-8 text-dark">Words (no spaces)</div>
+        <div class="col-4">
+          <span class="text-secondary">1,098</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<footer class="footer">
+  <div class="container p-3 align-middle">
+    <p class="text-center text-dark mt-3">© 2020 WriterViet</p>
+  </div>
+</footer>
